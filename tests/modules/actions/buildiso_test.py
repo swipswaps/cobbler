2bff36f4afbc994d027bb11554668f836c619021
SchoolGuy <egotthold@suse.de>
2020-09-25 16:45:52 +0200

    Tests: Add stubs for all tests

--- tests/modules/actions/buildiso_test.py
+++ tests/modules/actions/buildiso_test.py
@@ -18,21 +18,59 @@ class TestBuildiso:
     def test_make_shorter(self):
         # Arrange
         buildiso = BuildIso()
+
         # Act
+        buildiso.make_shorter()
+
         # Assert
         assert False
 
     def test_copy_boot_files(self):
+        # Arrange
+        buildiso = BuildIso()
+
+        # Act
+        buildiso.copy_boot_files()
+
+        # Assert
         assert False
 
     def test_filter_systems_or_profiles(self):
+        # Arrange
+        buildiso = BuildIso()
+
+        # Act
+        buildiso.filter_systems_or_profiles()
+
+        # Assert
         assert False
 
     def test_generate_netboot_iso(self):
+        # Arrange
+        buildiso = BuildIso()
+
+        # Act
+        buildiso.generate_netboot_iso()
+
+        # Assert
         assert False
 
     def test_generate_standalone_iso(self):
+        # Arrange
+        buildiso = BuildIso()
+
+        # Act
+        buildiso.generate_standalone_iso()
+
+        # Assert
         assert False
 
     def test_run(self):
+        # Arrange
+        buildiso = BuildIso()
+
+        # Act
+        buildiso.run()
+
+        # Assert
         assert False
